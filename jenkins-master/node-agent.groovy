47887834760801ad82c3eec66b24916b242fc6fa
huynhminhchu <34889054+huynhminhchu@users.noreply.github.com>
2021-08-21 17:18:28 +0700

    Create node-agent.groovy

---
+++ jenkins-master/node-agent.groovy
@@ -0,0 +1,20 @@
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.plugins.sshslaves.*;
+ 
+domain = Domain.global()
+store = Jenkins.instance
+.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0]
+getStore()
+ 
+slavesPrivateKey = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL,
+         "Jenkins-workers",
+         "Ec2-user",
+         new BasicSSHUserPrivateKey.UsersPrivateKeySource(),
+         "", "")
+store.addCredentials(domain, slavesPrivateKey)
+//creates SSH credentials based on the AWS Keypair we will use to deploy Jenkins workers instances
