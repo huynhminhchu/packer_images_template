167645ef7d1f865559ec5a15f4da1cc42b9830f8
Huynh Minh Chu <huynhminhchu1999@gmail.com>
2021-08-22 10:35:34 +0700

    completely fixed

--- jenkins-master/setup.sh
+++ jenkins-master/setup.sh
@@ -1,19 +1,29 @@
-#!/bin/bash 
-yum remove -y java yum install -y java-1.8.0-openjdk
-  chkconfig jenkins on
- 
+#!/bin/bash
+
+echo "Install Jenkins stable release"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install -y jenkins
+chkconfig jenkins on
+
+echo "INSTALL GIT"
 yum install -y git
-  mkdir /var/lib/jenkins/.ssh
-  touch /var/lib/jenkins/.ssh/known_hosts
-  chown -R jenkins:jenkins /var/lib/jenkins/.ssh
-  chmod 700 /var/lib/jenkins/.ssh
-  mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
-  chmod 600 /var/lib/jenkins/.ssh/id_rsa
-  chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
- 
+
+echo "SETUP SSH KEY"
+mkdir /var/lib/jenkins/.ssh
+touch /var/lib/jenkins/.ssh/known_hosts
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh
+chmod 700 /var/lib/jenkins/.ssh
+mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
+chmod 600 /var/lib/jenkins/.ssh/id_rsa
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
+
+echo "CONFIGURE JENKINS"
 mkdir -p /var/lib/jenkins/init.groovy.d
-  mv /tmp/.groovy /var/lib/jenkins/init.groovy.d/
-  mv /tmp/jenkins /etc/sysconfig/jenkins 
-  chmod +x /tmp/install-plugins.sh
-  bash /tmp/install-plugins.sh
-  service jenkins start
+mv /tmp/scripts/*.groovy /var/lib/jenkins/init.groovy.d/
+mv /tmp/config/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/config/install-plugins.sh
+bash /tmp/config/install-plugins.sh
+service jenkins start
